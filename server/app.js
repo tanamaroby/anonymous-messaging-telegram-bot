a81683de052ff040e00a5a359e108c05e77201a9
tanamaroby <tanamaroby@gmail.com>
2021-11-17 15:25:22 +0800

    Change wakeup method

--- server/app.js
+++ server/app.js
@@ -11,7 +11,6 @@ import logger from 'morgan';
 import indexRouter from './routes/index';
 
 const url = "https://whispering-plateau-19340.herokuapp.com/";
-const port = process.env.PORT || 80;
 
 const app = express();
 app.use(logger('dev'));
@@ -19,28 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 
-const wakeup = (url, interval = 25, callback) => {
-    const milliseconds = interval * 60000;
-    setTimeout(() => {
-        try {
-            console.log("setTimeout has been called.");
-            fetch(url).then(() => console.log("Fetching " + url));
-        } catch (err) {
-            console.log("Error fetching " + url + ", will try again in " + interval + " minutes");
-        } finally {
-            try {
-                callback();
-            } catch (err) {
-                callback ? console.log("Callback failed: ") : null;
-            } finally {
-                return wakeup(url, interval, callback);
-            }
-        }
-    }, milliseconds);
-}
-
-app.listen(port, () => {
-    wakeup(url);
-})
+// Prevent heroku from sleeping
+setInterval(function() {
+    fetch(url);
+}, 1200000);
 
 export default app;
